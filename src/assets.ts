17cf39b3abe3073d4d2605858d76efc41a06e799
1amageek <tmy0x3@icloud.com>
2018-03-10 09:57:00 +0900

    add comment

--- src/assets.ts
+++ src/assets.ts
@@ -1,20 +1,29 @@
 
 export default class Assets {
+    
+    // 29×29
     icon?: string
     icon2x?: string
 
+    // 160×50
     logo?: string
     logo2x?: string
 
+    // 180×220
     background?: string
     background2x?: string
 
+    // 90×90
     thumbnail?: string
     thumbnail2x?: string
 
+    // 286×15
     footer?: string
     footer2x?: string
-
+ 
+    // eventTicket: 375×98
+    // coupon: 375×144
+    // other: 375×123
     strip?: string
     strip2x?: string
 
